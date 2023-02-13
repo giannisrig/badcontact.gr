193a7605ba32b8575d5e718cee226b1a686c70d3
giannisrig <giannisrig93@gmail.com>
2023-02-13 15:53:07 +0200

    c

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
 
         <main className={fonts.grava400.className}>
 
-          <section className={styles.aboutSection}>
+            <section className={styles.aboutSection}>
 
             <Image className={styles.rocks} src={rocks} alt="Heavy Rock" />
 
@@ -59,45 +59,49 @@ export default function Home() {
 
           </section>
 
-          <section className={styles.singleSection}>
+            <section className={styles.singleSection}>
 
-            <div className={styles.illustrationContainer}>
+                <div className={styles.illustrationContainer}>
 
-              <Image src={singleBgIllustration} alt="Bad Contact Out of the Blue Background Illustration" title="Bad Contact Out of the Blue Background Illustration" />
-
-            </div>
-
-            <div className={styles.content}>
-
-                <Link href="/discography/out-of-the-blue" className={styles.artworkCover}>
-                    <Image src={coverArtworkImg} alt="Bad Contact - Out of the Blue (Single 2023) - Artwork Cover" title="Bad Contact - Out of the Blue (Single 2023) - Artwork Cover" width={315} height={315} quality={100} />
-                </Link>
-
-                <div className={styles.contentContainer}>
-
-                <div className={styles.titleContainer}>
-                    <h3 className={` ${styles.singleTitle} ${fonts.voltec.className}`}>
-                       OUT OF THE BLUE <span>(2023)</span>
-                    </h3>
-
-                    <h4 className={styles.feat}>
-                       Featuring Andreas Lagios from <a href={'https://nightstalkerband.com/'} rel="noreferrer" target={'_blank'}>Nightstalker</a> as a guest star on the bass.
-                    </h4>
-                </div>
-
-                <div className={styles.buttonsContainer}>
-
-                  <ButtonLink href="#" image="/images/spotify.svg" text="SPOTIFY" />
-                  <ButtonLink href="#" image="/images/bandcamp-bad-contact.svg" text="BANDCAMP" />
-                  <ButtonLink href="#" image="/images/youtube-bad-contact.svg" text="YOUTUBE" />
-                  <ButtonLink href="#" image="/images/apple-music-bad-contact.svg" text="APPLE MUSIC" />
-                  <ButtonLink href="#" image="/images/pandora-bad-contact.svg" text="PANDORA" />
+                  <Image src={singleBgIllustration} alt="Bad Contact Out of the Blue Background Illustration" title="Bad Contact Out of the Blue Background Illustration" />
 
                 </div>
 
-              </div>
+                <div className={styles.content}>
 
-            </div>
+                    <Link href="/discography/out-of-the-blue" className={styles.artworkCover}>
+                        <Image src={coverArtworkImg} alt="Bad Contact - Out of the Blue (Single 2023) - Artwork Cover" title="Bad Contact - Out of the Blue (Single 2023) - Artwork Cover" width={315} height={315} quality={100} />
+                    </Link>
+
+                    <div className={styles.contentContainer}>
+
+                    <div className={styles.titleContainer}>
+                        <h3 className={` ${styles.singleTitle} ${fonts.voltec.className}`}>
+                           OUT OF THE BLUE <span>(2023)</span>
+                        </h3>
+
+                        <h4 className={styles.feat}>
+                           Featuring Andreas Lagios from <a href={'https://nightstalkerband.com/'} rel="noreferrer" target={'_blank'}>Nightstalker</a> as a guest star on the bass.
+                        </h4>
+                    </div>
+
+                    <div className={styles.buttonsContainer}>
+
+                      <ButtonLink href="#" image="/images/spotify.svg" text="SPOTIFY" />
+                      <ButtonLink href="#" image="/images/bandcamp-bad-contact.svg" text="BANDCAMP" />
+                      <ButtonLink href="#" image="/images/youtube-bad-contact.svg" text="YOUTUBE" />
+                      <ButtonLink href="#" image="/images/apple-music-bad-contact.svg" text="APPLE MUSIC" />
+                      <ButtonLink href="#" image="/images/pandora-bad-contact.svg" text="PANDORA" />
+
+                    </div>
+
+                    <Link href="/discography/out-of-the-blue" className={` ${styles.readMore} ${fonts.voltec.className}`}>
+                       More information
+                    </Link>
+
+                  </div>
+
+                </div>
 
           </section>
 
