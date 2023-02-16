d4448a0941c850c2adfd10896ba2b960d8a28287
giannisrig <giannisrig93@gmail.com>
2023-02-16 19:50:02 +0200

    breadcrumbs change

--- components/breadcrumbs.js
+++ components/breadcrumbs.js
@@ -1,43 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import React from 'react';
+import {useRouter} from 'next/router';
 import Link from 'next/link';
 import fonts from "@/helpers/fonts";
 import styles from "@/styles/components/Breadcrumbs.module.scss";
 
 export default function Breadcrumbs() {
+
     const router = useRouter();
-    const [breadcrumbs, setBreadcrumbs] = useState(null);
     const interleave = (arr, thing) => [].concat(...arr.map(n => [n, thing])).slice(0, -1)
+    let breadcrumbs = null;
 
     function formatText(string) {
         string = string.replace(/-/g, ' ')
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
 
-    useEffect(() => {
-        if (router) {
-            const linkPath = router.asPath.split('/');
-            linkPath.shift();
+    if (router) {
+        const linkPath = router.asPath.split('/');
+        linkPath.shift();
 
-            const pathArray = interleave(
-                linkPath.map((path, i) => {
+        breadcrumbs = interleave(
+            linkPath.map((path, i) => {
 
-                    return { breadcrumb: path, type:'link', href: '/' + linkPath.slice(0, i + 1).join('/') };
-                    // if( (i + 1) === linkPath.length ){
-                    //     return { breadcrumb: path, type:'current', href: '/' + linkPath.slice(0, i + 1).join('/') };
-                    // }
-                    // else {
-                    //     return { breadcrumb: path, type:'link', href: '/' + linkPath.slice(0, i + 1).join('/') };
-                    // }
+                return {breadcrumb: path, type: 'link', href: '/' + linkPath.slice(0, i + 1).join('/')};
 
-                }), { breadcrumb: '/', type:'separator' }
-            );
+            }), {breadcrumb: '/', type: 'separator'}
+        );
 
-            setBreadcrumbs(pathArray);
-        }
-    }, [router]);
+    }
 
-    if (!breadcrumbs) {
+    if( !breadcrumbs ){
         return null;
     }
 
