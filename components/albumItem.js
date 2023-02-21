6a0991cb8e5ee0cb226a98d581bb5df2e9c9a85c
giannisrig <giannisrig93@gmail.com>
2023-02-21 16:24:48 +0200

    C

--- components/albumItem.js
+++ components/albumItem.js
@@ -4,7 +4,9 @@ import Image from "next/image";
 import fonts from '@/helpers/fonts'
 
 
-export default function AlbumItem({title,subtitle, link, img, release }) {
+export default function AlbumItem({title,subtitle, link, img, release, btnText }) {
+
+    const text = btnText ? btnText : 'VIEW MORE';
 
     return (
         <div className={styles.item}>
@@ -29,7 +31,7 @@ export default function AlbumItem({title,subtitle, link, img, release }) {
 
                     </div>
                     <Link href={link} className={styles.link}>
-                        <span className={fonts.grava700.className}>VIEW MORE</span>
+                        <span className={fonts.grava700.className}>{text}</span>
                         <svg  x="0px" y="0px" viewBox="0 0 512 512"><g><g><path d="M506.134,241.843c-0.006-0.006-0.011-0.013-0.018-0.019l-104.504-104c-7.829-7.791-20.492-7.762-28.285,0.068c-7.792,7.829-7.762,20.492,0.067,28.284L443.558,236H20c-11.046,0-20,8.954-20,20c0,11.046,8.954,20,20,20h423.557l-70.162,69.824c-7.829,7.792-7.859,20.455-0.067,28.284c7.793,7.831,20.457,7.858,28.285,0.068l104.504-104c0.006-0.006,0.011-0.013,0.018-0.019C513.968,262.339,513.943,249.635,506.134,241.843z"/></g></g></svg>
                     </Link>
                 </div>
--- pages/discography/index.js
+++ pages/discography/index.js
@@ -5,6 +5,7 @@ import PageHeader from "@/components/pageHeader";
 import Layout from "@/components/layout";
 import AlbumItem from "@/components/albumItem";
 import coverArtworkImg from "@/public/images/BadContact-OutOfTheBlue2023ArtworkCover.jpg";
+import newAlbumImg from "@/public/images/new-album.jpg";
 
 export default function Discography() {
     return (
@@ -17,6 +18,7 @@ export default function Discography() {
                     <PageHeader title='DISCOGRAPHY' subtitle='All official music releases of Bad Contact.' />
                 </div>
                 <AlbumItem title='OUT OF THE BLUE (2023)' subtitle='feat. Andreas Lagios (Nightstalker)' img={coverArtworkImg} link='/discography/out-of-the-blue/' release='SINGLE' />
+                <AlbumItem title='FULL ALBUM (2023/2024)' subtitle='Composing for the album is finished and we are on a pre-production phase. You can support us for our future release, by making a donation.' img={newAlbumImg} link='/donate/' btnText='DONATE' release='LP ALBUM' />
             </Layout>
         </>
     );
