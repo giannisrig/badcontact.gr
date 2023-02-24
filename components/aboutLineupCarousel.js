204d43a66e78198d328be18983df9059f2151372
giannisrig <giannisrig93@gmail.com>
2023-02-24 15:51:34 +0200

    c

---
+++ components/aboutLineupCarousel.js
@@ -0,0 +1,81 @@
+import styles from "@/styles/components/AboutLineupCarousel.module.scss";
+import Carousel from 'nuka-carousel/es/index';
+import Leonidas from "@/public/images/band/leonidas-zacharakis-vocals-bad-contact.jpg"
+import Giannis from "@/public/images/band/giannis-riganas-guitars-bad-contact.jpg"
+import Marios from "@/public/images/band/marios-fourlas-guitars-bad-contact.jpg"
+import Ninos from "@/public/images/band/panagiotis-ninos-drums-percussion-bad-contact.jpg"
+import Maria from "@/public/images/band/maria-leiloglou-bass-bad-contact.jpg"
+import Vangelis from "@/public/images/band/vangelis-galanis-synths-piano-bad-contact.jpg"
+import Image from "next/image";
+import fonts from "@/helpers/fonts"
+
+export default function AboutLineupCarousel() {
+
+    const lineUp = [
+        {
+            name: 'Leonidas Zacharakis',
+            role: 'Vocals',
+            img: Leonidas
+        },
+        {
+            name: 'Giannis Riganas',
+            role: 'Guitars',
+            img: Giannis
+        },
+        {
+            name: 'Marios Fourlas',
+            role: 'Guitars',
+            img: Marios
+        },
+        {
+            name: 'Panagiotis Ninos',
+            role: 'Drums',
+            img: Ninos
+        },
+        {
+            name: 'Maria Leiloglou',
+            role: 'Bass',
+            img: Maria
+        },
+        {
+            name: 'Vangelis Galanis',
+            role: 'Synths/Piano',
+            img: Vangelis
+        },
+    ]
+
+    const config = {
+        containerClassName: styles.container,
+        nextButtonClassName: styles.nextButton,
+        nextButtonText: '>',
+        pagingDotsClassName: styles.pagingDots,
+        pagingDotsContainerClassName: styles.pagingDotsContainer,
+        prevButtonClassName: styles.prevButton,
+        prevButtonText: '<'
+    }
+
+    return (
+        <div className={styles.slider}>
+            <Carousel wrapAround={true} slidesToShow={1} adaptiveHeight={true} cellAlign={'left'} defaultControlsConfig={config}>
+                {lineUp.map((itemData, i) => {
+                    return(
+                        <div key={i} className={styles.item} >
+                            <div className={styles.imgContainer} >
+                                <Image src={itemData.img} alt={'Bad Contact - '+ itemData.name + ' - ' + itemData.role } />
+                            </div>
+                            <div className={styles.info}>
+                                <div className={` ${styles.name} ${fonts.voltec.className}`}>
+                                    {itemData.name}
+                                </div>
+                                <div className={styles.role}>
+                                    {itemData.role}
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </Carousel>
+        </div>
+    );
+
+}
--- pages/about-us.js
+++ pages/about-us.js
@@ -6,6 +6,7 @@ import PageHeader from "@/components/pageHeader";
 import GroupPhoto from "@/public/images/band/Bad-Contact-BandGroupPhoto.jpg"
 import GroupPhoto2 from "@/public/images/band/Bad-Contact-BandGroupPhoto2.jpg"
 import Image from "next/image";
+import AboutLineupCarousel from "@/components/aboutLineupCarousel";
 
 export default function AboutUs() {
     return (
@@ -14,33 +15,72 @@ export default function AboutUs() {
                 <title>About the Band - Bad Contact</title>
             </Head>
             <Layout>
+
                 <div>
                     <PageHeader title='BAD CONTACT' subtitle='All you need to know about the band, our story and the current lineup.' />
                 </div>
+
                 <div className={styles.imgContainer}>
                     <Image src={GroupPhoto} alt={'Bad Contact - Band Group Photo'} title={'Bad Contact - Band Group Photo'} />
                 </div>
-                <h3 className={`${fonts.voltec.className} ${styles.title}`}>
-                    ABOUT US
-                </h3>
-                <p className={styles.text}>
-                    Bad Contact from Athens Greece, is a band formed to create a rousing impassioned music of heavy rock, reflecting the sound of the 80's era by combining music elements of the beloved motivational bands we grew up with, integrating a rock n roll attitude and conveying positive vibes through our art.
-                </p>
-                <div className={styles.imgContainer}>
-                    <Image src={GroupPhoto2} alt={'Bad Contact - Band Group Photo'} title={'Bad Contact - Band Group Photo'} />
+
+                <div className={styles.content}>
+
+                    <div className={styles.textBlock}>
+
+                        <h3 className={`${fonts.voltec.className} ${styles.title}`}>
+                            ABOUT US
+                        </h3>
+
+                        <p className={styles.text}>
+                            Bad Contact from Athens Greece, is a band formed to create a rousing impassioned music of heavy rock, reflecting the sound of the 80's era by combining music elements of the beloved motivational bands we grew up with, integrating a rock n roll attitude and conveying positive vibes through our art.
+                        </p>
+
+                    </div>
+
+                    <div className={styles.imgContainer}>
+                        <Image src={GroupPhoto2} alt={'Bad Contact - Band Group Photo'} title={'Bad Contact - Band Group Photo'} />
+                    </div>
+
+                    <div className={styles.textBlock}>
+
+                        <h3 className={`${fonts.voltec.className} ${styles.title}`}>
+                            OUR STORY
+                        </h3>
+
+                        <div className={styles.paragraphs}>
+
+                            <p className={styles.text}>
+                                Bad Contact is a fresh new band formed in 2022 by Giannis, Marios and Panagiotis, friends who have been playing with each other in various bands for many years but never in the same one altogether.
+                            </p>
+
+                            <p className={styles.text}>
+                                The band started composing original music since day zero and after a couple of months and some lineup changes, the material for the first album was finished.
+                            </p>
+
+                            <p className={styles.text}>
+                                In 2023 Bad Contact released their first official music 'Out of the Blue' a single record featuring Andreas Lagios from Nightstalker as a special guest on the bass.
+                            </p>
+
+                        </div>
+
+                    </div>
+
+                    <div className={styles.textBlock}>
+
+                        <h3 className={`${fonts.voltec.className} ${styles.title}`}>
+                            LINEUP
+                        </h3>
+
+                        <p className={styles.text}>
+                            The current lineup of Bad Contact:
+                        </p>
+
+                        <AboutLineupCarousel />
+
+                    </div>
+
                 </div>
-                <h3 className={`${fonts.voltec.className} ${styles.title}`}>
-                    OUR STORY
-                </h3>
-                <p className={styles.text}>
-                    Bad Contact is a fresh new band formed in 2022 by Giannis, Marios and Panagiotis, friends who have been playing with each other in various bands for many years but never in the same one altogether.
-                </p>
-                <p className={styles.text}>
-                    The band started composing original music since day zero and after a couple of months and some lineup changes, the material for the first album was finished.
-                </p>
-                <p className={styles.text}>
-                    In 2023 Bad Contact released their first official music 'Out of the Blue' a single record featuring Andreas Lagios from Nightstalker as a special guest on the bass.
-                </p>
 
             </Layout>
         </>
