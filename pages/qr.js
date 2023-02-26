069b6055d3c925d0cd4c975737c3375668880493
giannisrig <giannisrig93@gmail.com>
2023-02-26 20:03:01 +0200

    c

--- pages/qr.js
+++ pages/qr.js
@@ -1,15 +1,31 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router'
 import Head from "next/head";
 import Layout from "@/components/layout";
 
 export default function Qr() {
 
+    const [pixelSent, setPixelSent] = useState(false);
     const router = useRouter();
 
     useEffect(() => {
+        import('react-facebook-pixel')
+            .then((x) => x.default)
+            .then((ReactPixel) => {
+                ReactPixel.init('804765880666454') // facebookPixelId
+                ReactPixel.pageView()
+
+                router.events.on('routeChangeComplete', () => {
+                    ReactPixel.pageView()
+                    setPixelSent(true);
+                })
+            })
+    }, [router.events])
+
+
+    useEffect(() => {
         router.push('/');
-    }, []);
+    }, [pixelSent]);
 
     return (
         <>
