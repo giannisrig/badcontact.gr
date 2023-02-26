68bc5c8f3bf14759c9fa3ecdb02922345c3591ad
giannisrig <giannisrig93@gmail.com>
2023-02-26 18:08:32 +0200

    c

---
+++ pages/qr.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router'
+import Head from "next/head";
+import Layout from "@/components/layout";
+
+export default function qr() {
+
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push('/');
+    }, []);
+
+    return (
+        <>
+            <Head>
+                <title>QR Short Link - Bad Contact</title>
+                <meta name="robots" content="noindex" />
+            </Head>
+            <Layout>
+
+            </Layout>
+        </>
+    );
+
+}
