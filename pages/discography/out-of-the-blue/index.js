3e4edd29422e7d668a79b6ba8bd2b3e7f72ec930
giannisrig <giannisrig93@gmail.com>
2023-03-05 14:03:15 +0200

    c

--- pages/discography/out-of-the-blue/index.js
+++ pages/discography/out-of-the-blue/index.js
@@ -95,7 +95,7 @@ export default function OutOfTheBlue() {
 
                             <ButtonLink href="https://open.spotify.com/album/3KtsGWIZ3O1IlQULcWsUei" image="/images/spotify.svg" text="SPOTIFY" />
                             <ButtonLink href="https://badcontact.bandcamp.com/album/out-of-the-blue" image="/images/bandcamp-bad-contact.svg" text="BANDCAMP" />
-                            <ButtonLink href="https://www.youtube.com/@badcontact" image="/images/youtube-bad-contact.svg" text="YOUTUBE" />
+                            <ButtonLink href="https://youtu.be/HIao6tbW-5I" image="/images/youtube-bad-contact.svg" text="YOUTUBE" />
                             <ButtonLink href="https://music.apple.com/us/album/out-of-the-blue-feat-andreas-lagios-single/1674087605" image="/images/apple-music-bad-contact.svg" text="APPLE MUSIC" />
                             <ButtonLink href="https://pandora.app.link/YHOJW136Mxb" image="/images/pandora-bad-contact.svg" text="PANDORA" />
 
